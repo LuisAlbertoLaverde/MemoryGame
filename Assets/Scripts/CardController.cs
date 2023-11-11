ef71b22fed7540467c4a17b7f8c12daa0e5b0a5b
Unumnio <unumnio@gmail.com>
2023-11-11 07:40:05 -0500

    I create CardController and LevelController scripts

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CardController : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> prefabs;
+
+    public UnityEvent<CardController> OnClicked;
+
+    public int CardType = -1;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        if (CardType <0)
+        {
+            CardType = UnityEngine.Random.Range(0, prefabs.Count);
+        }
+
+        Instantiate(prefabs[CardType],transform.position,quaternion.identity, transform);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        OnClicked.Invoke(this);
+    }
+
+    public void TestAnimation()
+    {
+        IEnumerator AnimationCoroutine()
+        {
+            Reveal();
+            yield return new WaitForSeconds(2);
+            Hide();
+        }
+
+        StartCoroutine(AnimationCoroutine());
+    }
+
+}
